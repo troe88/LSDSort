27babcffa3659c85ef25501680a4db03d28b18f2
dmitry <dmitry.trok@gmai.com>
2015-03-16 02:13:07 +0300

    exception handling(if mvn not found) added 2 run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
+
+if [ ! `which mvn 2>/dev/null` ] ; 
+then
+echo " no maven " 
+exit 1
+fi
+
 mvn -Dtest=OneThousandTest,HundredThousandTest,NegHundredThousandTest,MillionTest,NegMillionTest,TenMillionTest,NegTenMillionTest test | awk '/##/ {print}'
 
 
